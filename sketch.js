d663158bdbd2bda7f5538f91c97a136306de2742
memerrrrrrr1 <94204656+memerrrrrrr1@users.noreply.github.com>
2022-06-22 15:39:13 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("#4828D4");
+  fill("rgb(197,54,54)");
+  rect(42, 43, 100, 50);
+  circle(330, 67, 67);
+  ellipse(200, 200, 550, 55);
+}
